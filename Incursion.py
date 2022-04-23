838c8183a084e7ba315f58fac0f8c9a1aea945f6
ChadyPCh <88913382+ChadyPCh@users.noreply.github.com>
2022-04-23 12:04:55 -0400

    Update Incursion.py

--- Incursion.py
+++ Incursion.py
@@ -526,7 +526,7 @@ def porcen(p,t):
 def conn ():
 		
 	def __init__(self):
-		con=self.connection = pymysql.connect(
+		con = pymysql.connect(
 			host='containers-us-west-42.railway.app',
 			user='root',
 			password='qzNJ5WZvOxsZK8Pm8g7d',
@@ -534,7 +534,7 @@ def conn ():
 		)
 		print("se conectooooooo")
 				
-		cur=self.cursor = self.connection.cursor()	
+		cur = con.cursor()	
 		return con , cur
 	
 def SAVE_STAT_FORAY_SUCCES(Update,context):
